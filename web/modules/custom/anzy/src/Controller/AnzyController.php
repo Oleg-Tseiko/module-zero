9a43cdde9b4c5b87b03ee20f976addabc61c6d07
Oleg <O.Tseiko@gmail.com>
2021-05-26 13:16:49 +0300

    added routing and info.yml

---
+++ web/modules/custom/anzy/src/Controller/AnzyController.php
@@ -0,0 +1,23 @@
+<?php
+namespace Drupal\anzy\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Provides route responses for the Anzy module.
+ */
+class AnzyController extends ControllerBase {
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function catsPage() {
+    return [
+      '#markup' => '<h2>Hello! You can add here a photo of your cat.</h2>',
+    ];
+  }
+
+}
